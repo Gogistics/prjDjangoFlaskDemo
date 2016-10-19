5a986fae0ae42f062122148f661c4ae02b28bcca
Alan Tai <gogistics@gogistics-tw.com>
2016-10-19 11:07:48 -0700

    refactor routes of socketio

--- web_flask/app.py
+++ web_flask/app.py
@@ -2,36 +2,25 @@
 from flask import Flask, render_template, request, session
 from flask_sqlalchemy import SQLAlchemy
 from datetime import timedelta
-from flask_socketio import SocketIO, emit
+# from flask_socketio import SocketIO, emit
 from base64 import b64encode
-import os, time, random
 from models.user_models import UserModelsManager
+import os, time
+from routes.socketio_routes import MySocketIO
 
+# set app
 app = Flask(__name__)
 app.secret_key = b64encode(os.urandom(32)).decode('utf-8')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
-# set socketio
-async_mode = None
-thread = None
-socketio = SocketIO(app, async_mode = async_mode)
-
-# example 
-def background_thread():
-  """Example of how to send server generated events to clients."""
-  count = 0
-  while True:
-    socketio.sleep(1)
-    count += 1
-    socketio.emit('my_realtime_data', {'data': 'Server generated event', 'count': count, 'random_number': random.randint(1,10)}, namespace='/test')
-
-
-# set connection with postgres
+# set db connection with postgres
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://myprojectuser:helloDjango@172.17.0.2:5432/myproject'
 db = SQLAlchemy(app)
 UserModelsManager = UserModelsManager(db)
 User = UserModelsManager.get_user_model() # get User model
 
+# set SocketIO
+my_socketio = MySocketIO(app)
 
 # routes
 @app.route('/')
@@ -42,7 +31,7 @@ def index():
     
   print '<--- session --->'
   print str(session['auth_base'])
-  return render_template('index.html', async_mode = socketio.async_mode)
+  return render_template('index.html', async_mode = my_socketio.get_socketio().async_mode)
 
 @app.route('/prereg', methods = ['POST'])
 def prereg():
@@ -63,29 +52,7 @@ def partial(sub_template):
   template_path = 'my_ng_templates/{sub_template}'.format(sub_template = sub_template)
   return render_template(template_path, jinja_var='jinja sub-template')
 
-# socket
-@socketio.on('connect', namespace='/test')
-def test_connect():
-  print('client connected')
-
-  # example of background task for front-end real-time chart
-  global thread
-  if thread is None:
-    thread = socketio.start_background_task(target = background_thread)
-  
-  emit('my_response', {'data': 'Connected'})
-
-@socketio.on('disconnect', namespace='/test')
-def test_disconnect():
-  print('client disconnected')
-
-@socketio.on('my_event', namespace='/test')
-def test_message(message):
-  print('<--- my_event of socketio --->')
-  print str(message)
-  emit('my_response', {'data': message['data']})
-
 # entry
 if __name__ == '__main__':
   # app.run(debug=True,host='0.0.0.0',port=5000)
-  socketio.run(app, host='0.0.0.0', port=5000, debug=True)
+  my_socketio.get_socketio().run(app, host='0.0.0.0', port=5000, debug=True)
---
+++ web_flask/routes/socketio_routes.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+from flask_socketio import SocketIO, emit
+import random
+
+class MySocketIO(object):
+  def __init__(self, app):
+    self.__async_mode = None
+    self.__thread = None
+    self.__socketio = SocketIO(app, async_mode = self.__async_mode)
+
+    # example 
+    def background_thread():
+      """Example of how to send server generated events to clients."""
+      count = 0
+      while True:
+        self.__socketio.sleep(1)
+        count += 1
+        self.__socketio.emit('my_realtime_data', {'data': 'Server generated event', 'count': count, 'random_number': random.randint(1,10)}, namespace='/test')
+
+    # socket routes
+    @self.__socketio.on('connect', namespace='/test')
+    def test_connect():
+      print('client connected')
+
+      # example of background task for front-end real-time chart
+      if self.__thread is None:
+        self.__thread = self.__socketio.start_background_task(target = background_thread)
+      
+      emit('my_response', {'data': 'Connected'})
+
+    @self.__socketio.on('disconnect', namespace='/test')
+    def test_disconnect():
+      print('client disconnected')
+
+    @self.__socketio.on('my_event', namespace='/test')
+    def test_message(message):
+      print('<--- my_event of socketio --->')
+      print str(message)
+      emit('my_response', {'data': message['data']})
+
+  def get_socketio(self):
+    return self.__socketio
